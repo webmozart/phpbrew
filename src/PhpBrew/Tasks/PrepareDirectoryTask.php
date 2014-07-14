ef8561a6462d388408ded268b4f47cd49a702427
c9s <yoanlin93@gmail.com>
2014-07-14 14:34:36 +0800

    refactoring prepare directory task

--- src/PhpBrew/Tasks/PrepareDirectoryTask.php
+++ src/PhpBrew/Tasks/PrepareDirectoryTask.php
@@ -7,21 +7,13 @@ class PrepareDirectoryTask extends BaseTask
 {
     public function prepareForVersion($version)
     {
-        $home = Config::getPhpbrewRoot();
-        $buildDir = Config::getBuildDir();
-        $variantsDir = Config::getVariantsDir();
-        $buildPrefix = Config::getVersionBuildPrefix($version);
-
-        if (!file_exists($variantsDir)) {
-            mkdir($variantsDir, 0755, true);
-        }
-
-        if (!file_exists($buildDir)) {
-            mkdir($buildDir, 0755, true);
-        }
-
-        if (!file_exists($buildPrefix)) {
-            mkdir($buildPrefix, 0755, true);
+        $dirs = array();
+        $dirs[] = Config::getPhpbrewRoot();
+        $dirs[] = Config::getBuildDir();
+        $dirs[] = Config::getVariantsDir();
+        $dirs[] = Config::getVersionBuildPrefix($version);
+        foreach($dirs as $dir) {
+            mkdir($dir, 0755, true);
         }
     }
 }
