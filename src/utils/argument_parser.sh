ae17c471b90e43b534362ab79f409f48cdbc9caf
Alexander Schneider <alexander.schneider@jankowfsky.com>
2014-07-21 00:41:41 +0200

    Improve code Grep some ideas from docopt and php-CLIFramework

--- src/utils/argument_parser.sh
+++ src/utils/argument_parser.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 key_exists() {
-  if [ "$2" != in ]; then
-    echo "Incorrect usage."
-    echo "Correct usage: key_exists {key} in {array}"
-    return
-  fi
-
-  eval '[ ${'$3'[$1]+key_exists} ]'
+  eval '[ ${'$2'[$1]+key_exists} ]'
 }
 
 is_option() {
@@ -17,90 +12,155 @@ is_option() {
 
   return 1
 }
+
 parse_arguments() {
   command=''
-  declare -g -A required_arguments
-  required_arguments=()
-  declare -g -A optional_arguments
-  optional_arguments=()
+  declare -g -A command_arguments=()
 
-  local known_arguments known_options raw_arguments
+  #get function agruments
+  local known_arguments=("${!1}")
+  local known_options=("${!2}")
+  local raw_arguments=("${!3}")
 
-  known_arguments=("${!1}")
-  known_options=("${!2}")
-  raw_arguments=("${!3}")
 
-  local number_required_arguments no_raw_arguments current_arg counter
+  #get definied options
+  local opt_map opt_type_map
+  declare -A opt_map=()
+  declare -A opt_type_map=()
 
-  number_required_arguments=${#known_arguments[@]}
-  no_raw_arguments=${#raw_arguments[@]}
-  current_arg=''
-  counter=0
+  for opt in "${known_options[@]}"; do
+    local full_opt_name=''
+    local opt_names=( $(echo ${opt} | grep -o '[a-z0-9\-]*') )
+
+    if [[ ${#opt_names[@]} == 2 ]]; then
+      full_opt_name="--${opt_names[1]}"
+      opt_map["-${opt_names[0]}"]=${full_opt_name}
+    elif [[ ${#opt_names[@]} == 1 ]]; then
+      full_opt_name="--${opt_names[0]}"
+      opt_map[${full_opt_name}]=${full_opt_name}
+    else
+      echo "Error wrong defined options"
+      exit 1
+    fi
+
+    local opt_type=$(echo ${opt} | grep -o '[:?+]')
+
+    if [[ ${opt_type} == "" ]]; then
+      echo "Error no option type given for ${full_opt_name}"
+      exit 1
+    fi
+
+    opt_type_map[${full_opt_name}]=${opt_type}
+  done
+
+
+  #iterate over the raw argumgents
+  local number_command_arguments=${#known_arguments[@]}
+  local no_raw_arguments=${#raw_arguments[@]}
+  local arg=''
+  local current_arg=''
+  local counter=0
+  local req_param_name=''
 
   while [[ ${counter} -lt ${no_raw_arguments} ]]; do
-    arg="${raw_arguments["${counter}"]}"
+    arg=${raw_arguments[${counter}]}
 
     if [[ ${counter} == 0 ]] && !(is_option ${arg}); then
       command=${arg}
-    elif [[ ${counter} -le ${number_required_arguments} ]] && !(is_option ${arg}); then
-      required_arguments=("${required_arguments[@]}" ${arg})
-    elif [[ ${counter} -gt ${number_required_arguments} ]] && is_option ${arg}; then
-      current_arg=`expr "${arg}" : '-*\([a-zA-Z]*\)'`
-
-      if !(key_exists ${current_arg} in optional_arguments); then
-        optional_arguments["${current_arg}"]=''
+    elif [[ ${counter} -le ${number_command_arguments} ]] && !(is_option ${arg}); then
+      req_param_name=${known_arguments[$((counter - 1))]}
+      command_arguments[${req_param_name}]=${arg}
+    elif [[ ${counter} -gt ${number_command_arguments} ]] && is_option ${arg}; then
+      if (key_exists ${arg} opt_map); then
+        arg=${opt_map[${arg}]}
+      else
+        echo "Invalid option ${arg}"
+        exit 1
       fi
 
-      local next
-      let next=counter+1
-      arg="${raw_arguments["${next}"]}"
+      current_arg=${arg}
+      local is_multiple_opt=false
 
-      if [[ ${arg} != "" ]] && !(is_option ${arg}); then
-        if [[ "${optional_arguments["${current_arg}"]}" != "" ]]; then
-          optional_arguments["${current_arg}"]+="|"
+      if [[ ${opt_type_map[$current_arg]} == '+' ]]; then
+        is_multiple_opt=true
+      fi
+
+      if ${is_multiple_opt} && !(key_exists "${current_arg},#" command_arguments); then
+        command_arguments["${current_arg},#"]=0
+      elif !(${is_multiple_opt}) && !(key_exists ${current_arg} command_arguments); then
+        command_arguments[${current_arg}]=''
+      elif (key_exists ${current_arg} command_arguments) && [[ ${is_multiple_opt} ]]; then
+        echo "Error ${current_arg} can't be multiple definied"
+        exit 1
+      fi
+
+      local next=$((counter + 1))
+
+      if [[ ${next} -lt ${no_raw_arguments} ]]; then
+        arg=${raw_arguments[${next}]}
+
+        if [[ ${arg} != "" ]] && !(is_option ${arg}); then
+          if ${is_multiple_opt}; then
+            local no_opt_args=${command_arguments["${current_arg},#"]}
+            command_arguments["${current_arg},${no_opt_args}"]="${arg}"
+            command_arguments["${current_arg},#"]=$((no_opt_args+1))
+          else
+            command_arguments[${current_arg}]="${arg}"
+          fi
+
+          counter=${next}
         fi
-
-        optional_arguments["${current_arg}"]+="${arg}"
-        let counter=next
       fi
     else
       if [[ ${counter} == 0 ]]; then
         echo "invalide command ${arg}"
-      elif [[ ${counter} -le ${number_required_arguments} ]]; then
-        req_param_name=known_arguments[0]
+      elif [[ ${counter} -le ${number_command_arguments} ]]; then
+        req_param_name=${known_arguments[$((counter - 1))]}
         echo "missing required parameter ${req_param_name}"
-      elif [[ ${counter} -gt ${number_required_arguments} ]]; then
+      elif [[ ${counter} -gt ${number_command_arguments} ]]; then
         echo "wrong option ${arg}"
       fi
 
       exit 1
     fi
 
-    let counter=counter+1
+    counter=$((counter + 1))
   done
 }
 
-args=("$@")
-known_arguments=('name' 'version')
-known_options=(
-  '-t/--test|desc'
-)
-parse_arguments known_arguments[@] command_options[@] args[@]
-
 #test
+command_args=(
+  'name'
+  'version'
+)
+sub_commands=(
+  ''
+)
+command_ops=(
+  'single-required:'
+  'single-optional?'
+  'single-repeatable+'
+  'r|required-opt:'
+  'o|optional-opt?'
+  'p|repeatable-opt+'
+)
+args=("$@")
+
+parse_arguments command_args[@] command_ops[@] args[@]
+
 echo "--- Command ---"
 echo "${command}"
-echo "--- Required args ---"
-for req_arg in "${required_arguments[@]}"; do
-  echo ${req_arg}
-done
+
 echo "---- Args -----"
 
-for k in "${!optional_arguments[@]}"; do
+for k in "${!command_arguments[@]}"; do
   echo "${k}: "
-  IFS='|' read -a params <<< "${optional_arguments["${k}"]}"
+  params=()
+  IFS='|' read -a params <<< "${command_arguments["${k}"]}"
 
-  for param in "${params[@]}"; do
-      echo "-- ${param}"
-  done
+  if [ ${#params[@]} -gt 0 ]; then
+    for param in "${params[@]}"; do
+        echo "${param}"
+    done
+  fi
 done
